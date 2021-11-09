b89a2ad226555422add6a3a7ad20026e3068e2ca
Samuel Rulli <samuel.rulli@gmail.com>
2021-11-08 18:59:03 -0800

    W1-A1

---
+++ W1-A1/index.php
@@ -0,0 +1,11 @@
+<!-- http://localhost/web205/web205/W1-A1/index.php -->
+<html>
+    <head>
+    <title>PHP - W1-A1</title>
+    </head>
+    <body>
+        <?php 
+            echo '<p>Hello World</p>'; 
+        ?> 
+    </body>
+</html>
